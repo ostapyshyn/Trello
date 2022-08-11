1cb881cd6b1e623f13651d64f7aaaa8dd13b44de
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-08-11 14:40:45 +0300

    Add ability to change name in portfolio

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { getAuth } from 'firebase/auth';
+import React, { useState } from 'react';
+import { getAuth, updateProfile } from 'firebase/auth';
+import { updateDoc, doc } from 'firebase/firestore';
+import { db } from '../lib/init-firebase';
 import { useNavigate, Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { toast } from 'react-toastify';
 
 const Profile = () => {
   const auth = getAuth();
+  const [changeDetails, setChangeDetails] = useState(false);
   const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
@@ -18,21 +22,115 @@ const Profile = () => {
     navigate('/');
   };
 
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+        const userRef = doc(db, 'users', auth.currentUser.uid);
+        await updateDoc(userRef, {
+          name,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error('Could not update profile details');
+    }
+  };
+
   return (
     <div className="profile">
       <header className="profileHeader">
         <PageHeaderParagraph>My profile</PageHeaderParagraph>
-        <button type="button" className="logOut" onClick={onLogout}>
+        <LogOutButton type="button" onClick={onLogout}>
           Logout
-        </button>
+        </LogOutButton>
       </header>
+
+      <main>
+        <ProfileHeader>
+          <ProfileDetailsTextP>Details</ProfileDetailsTextP>
+          <ChangePersonalDetailsP
+            onClick={() => {
+              changeDetails && onSubmit();
+              setChangeDetails((prevState) => !prevState);
+            }}
+          >
+            {changeDetails ? 'done' : 'change'}
+          </ChangePersonalDetailsP>
+        </ProfileHeader>
+
+        <ProfileCard>
+          <form>
+            <input
+              type="text"
+              id="name"
+              className={!changeDetails ? 'profileName' : 'profileNameActive'}
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChange}
+            />
+            <input
+              type="text"
+              id="email"
+              className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
+              disabled={!changeDetails}
+              value={email}
+              onChange={onChange}
+            />
+          </form>
+        </ProfileCard>
+      </main>
     </div>
   );
 };
 
+const ChangePersonalDetailsP = styled.p`
+  cursor: pointer;
+  font-weight: 600;
+  color: #00cc66;
+`;
+
+const ProfileDetailsTextP = styled.p`
+  font-weight: 600;
+`;
+
 const PageHeaderParagraph = styled.p`
   font-size: 2rem;
   font-weight: 800;
+`;
+
+const ProfileCard = styled.div`
+  background-color: #ffffff;
+  border-radius: 1rem;
+  padding: 1rem;
+  box-shadow: 3px 0px 11px 0px rgba(0, 0, 0, 0.2);
+  max-width: 500px;
+`;
+
+const ProfileHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  max-width: 500px;
+`;
+
+const LogOutButton = styled.div`
+  cursor: pointer;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 1rem;
+  font-weight: 600;
+  color: #ffffff;
+  background-color: #00cc66;
+  padding: 1rem 0.75rem;
+  border-radius: 1rem;
 `;
 
 export default Profile;
