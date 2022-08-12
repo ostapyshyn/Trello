51da60cfc79b8e81de7cdb5fb9dd15462b501824
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-08-12 13:03:31 +0300

    Fix about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-
+import styled from 'styled-components';
 const AboutPage = () => {
   return (
-    <div style={{ textAlign: 'center' }}>
+    <ProfileCard>
       <h1>About us.</h1>
       <h2>Hello There!</h2>
       <p>This is a demo Trello project!</p>
-    </div>
+    </ProfileCard>
   );
 };
+
+const ProfileCard = styled.div`
+  text-align: center;
+  margin: 0 auto;
+  background-color: #ffffff;
+  border-radius: 1rem;
+  padding: 1rem;
+  box-shadow: 3px 0px 11px 0px rgba(0, 0, 0, 0.2);
+  max-width: 500px;
+  margin-top: 4rem;
+`;
 
 export default AboutPage;
