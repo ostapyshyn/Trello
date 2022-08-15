0db216ab5094d32ffb10fb7a053e5d4d0b5f0a6d
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-08-15 18:19:01 +0300

    Create board

--- src/pages/CreateBoard.jsx
+++ src/pages/CreateBoard.jsx
@@ -1,7 +1,97 @@
-import React from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { db } from '../lib/init-firebase';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { v4 as uuidv4 } from 'uuid';
+import Spinner from '../components/Spinner';
 
-const CreateBoard = () => {
-  return <div>Create a Board</div>;
-};
+function CreateBoard() {
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '',
+  });
+  const { name } = formData;
+
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    if (isMounted) {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setFormData({ ...formData, userRef: user.uid });
+        } else {
+          navigate('/sign-in');
+        }
+      });
+    }
+    return () => {
+      isMounted.current = false;
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isMounted]);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const formDataCopy = {
+      ...formData,
+      timestamp: serverTimestamp(),
+    };
+
+    const docRef = await addDoc(collection(db, 'boards'), formDataCopy);
+    setLoading(false);
+    toast.success('Board saved');
+    navigate(`/boards/${docRef.id}`);
+  };
+
+  const onMutate = (e) => {
+    if (!e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        [e.target.id]: e.target.value,
+      }));
+    }
+  };
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <div className="profile">
+      <header>
+        <p className="pageHeader">Create a Board</p>
+      </header>
+
+      <main>
+        <form onSubmit={onSubmit}>
+          <label className="formLabel">Sell / Rent</label>
+
+          <label className="formLabel">Name</label>
+          <input
+            className="formInputName"
+            type="text"
+            id="name"
+            value={name}
+            onChange={onMutate}
+            maxLength="32"
+            minLength="10"
+            required
+          />
+
+          <button type="submit" className="primaryButton createListingButton">
+            Create a Board
+          </button>
+        </form>
+      </main>
+    </div>
+  );
+}
 
 export default CreateBoard;
