5c9f608cdfcdd024227236294a80e4e6d4408731
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-08-15 11:08:25 +0300

    Add button create board

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg';
+import trelloBoard from '../assets/svg/trelloBoard.svg';
+import styled from 'styled-components';
 
 const HomePage = () => {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>Homepage here</h1>
-      <p>Show list of user boards, with ability to add new board!</p>
-    </div>
+    <Wrapper>
+      <Link to="/create-board">
+        <CreateBoard>
+          <img src={trelloBoard} alt="home" />
+          <p>Create board</p>
+          <img src={arrowRight} alt="arrow right" />
+        </CreateBoard>
+      </Link>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  padding: 1rem;
+`;
+
+const CreateBoard = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: #ffffff;
+  max-width: 500px;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 1rem;
+  padding: 0.25rem 1rem;
+  font-weight: 600;
+`;
 
 export default HomePage;
