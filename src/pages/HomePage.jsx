f38aab554aba598f377d217b24e0941c0a17800b
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-08-15 12:28:13 +0300

    Show boards from firebase

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg';
 import trelloBoard from '../assets/svg/trelloBoard.svg';
 import styled from 'styled-components';
+import { db } from '../lib/init-firebase';
+import { toast } from 'react-toastify';
+import Spinner from '../components/Spinner';
+import { collection, getDocs, query, where, orderBy, limit, startAfter } from 'firebase/firestore';
 
 const HomePage = () => {
+  const [boards, setBoards] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchBoards = async () => {
+      try {
+        const boardsRef = collection(db, 'boards');
+        const q = query(boardsRef);
+        const querySnap = await getDocs(q);
+
+        const boards = [];
+
+        querySnap.forEach((doc) => {
+          return boards.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+
+        setBoards(boards);
+        setLoading(false);
+      } catch (error) {
+        toast.error('Could not fetch listings');
+      }
+    };
+
+    fetchBoards();
+  }, []);
+
   return (
     <Wrapper>
       <Link to="/create-board">
@@ -14,6 +49,22 @@ const HomePage = () => {
           <img src={arrowRight} alt="arrow right" />
         </CreateBoard>
       </Link>
+
+      {loading ? (
+        <Spinner />
+      ) : boards && boards.length > 0 ? (
+        <>
+          <main>
+            <ul className="categoryListings">
+              {boards.map((board) => (
+                <h3>{board.data.name}</h3>
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>There are no boards yet!</p>
+      )}
     </Wrapper>
   );
 };
