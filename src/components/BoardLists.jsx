3c2ae287b46c0fa3466e8f69f7b86954eb06baff
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-08-16 14:09:06 +0300

    Add fetchListing

--- src/components/BoardLists.jsx
+++ src/components/BoardLists.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { getDoc, doc } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { db } from '../lib/init-firebase';
+import Spinner from '../components/Spinner';
+import shareIcon from '../assets/svg/shareIcon.svg';
 
 const BoardLists = () => {
-  return <div>Board Lists</div>;
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
+
+  const navigate = useNavigate();
+  const params = useParams();
+  const auth = getAuth();
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      console.log(params.listingId);
+      const docRef = doc(db, 'boards', params.board);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        setListing(docSnap.data());
+        setLoading(false);
+      }
+    };
+
+    fetchListing();
+  }, [navigate, params.listingId]);
+
+  return <div>Board Lists here</div>;
 };
 
 export default BoardLists;
