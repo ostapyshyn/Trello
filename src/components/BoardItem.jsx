8eb535548e74e2b90cb907adc4dbfa3e14de8758
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-08-18 11:01:52 +0300

    Add style to the board

--- src/components/BoardItem.jsx
+++ src/components/BoardItem.jsx
@@ -25,4 +25,11 @@ const BoardItemList = styled.li`
   align-items: center;
   margin-bottom: 1rem;
   position: relative;
+  cursor: pointer;
+  transition: box-shadow 0.2s ease-in-out, transform 0.2s ease-in-out;
+
+  &:hover {
+    box-shadow: 0px 20px 35px rgba(5, 5, 5, 0.06);
+    transform: translateY(-5px);
+  }
 `;
