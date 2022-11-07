42939bf4ec0f8d4b2425c82c8c58378c427288e2
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-11-07 22:28:09 +0200

    Add new tests

--- src/components/Navbar/__test__/Navbar.test.js
+++ src/components/Navbar/__test__/Navbar.test.js
@@ -2,8 +2,20 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Navbar from '../index.tsx';
 
-it('should check title', async () => {
+// it('should check title', async () => {
+//   render(<Navbar title="My Trello" />);
+//   const headingElement = screen.getByText(/my trello/i);
+//   expect(headingElement).toBeInTheDocument();
+// });
+
+it('should check role', async () => {
   render(<Navbar title="My Trello" />);
-  const headingElement = screen.getByText(/my trello/i);
+  const headingElement = screen.getByRole('heading', { name: /my trello/i });
   expect(headingElement).toBeInTheDocument();
+});
+
+it('should check word', async () => {
+  render(<Navbar title="My Trello" />);
+  const headingElement = screen.queryByText(/start/i);
+  expect(headingElement).not.toBeInTheDocument();
 });
